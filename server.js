d58aa9f8c6796c04c2f710b8d8322ea715227bec
NdikumNdifor <sebastienndifor@gmail.com>
2024-06-20 11:41:27 +0200

    Inventory delivery by classification completed.

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+// const utilities = require("./utilities")
 const app = express()
 const static = require("./routes/static")
 
@@ -30,6 +32,28 @@ app.use(static)
 *********************/
 app.get("/", baseController.buildHome)
 
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+// app.use(async (req, res, next) => {
+//   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+//  })
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+// app.use(async (err, req, res, next) => {
+//   let nav = await utilities.getNav()
+//   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+//   res.render("errors/error", {
+//   title: err.status || 'Server Error',
+//   message: err.message,
+//   nav
+//   })
+//  })
+ 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -43,3 +67,5 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
