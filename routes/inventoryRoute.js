b114b84c32dbefff2bcde1e0648e7446753e4db3
NdikumNdifor <sebastienndifor@gmail.com>
2024-07-24 02:49:35 +0200

    Select inventory item functionality added

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,7 +2,35 @@ const express = require("express")
 const router = new express.Router()
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/")
+const regValidate = require('../utilities/addInventory-validation')
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+
+// Routes to management view
+router.get("/inv", utilities.handleErrors(invController.buildManagement));
+
+// Route, that works with the URL in the JavaScript file "inventory.js" built
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
+
+// Route that matches the path and 
+// parameter to the end of the route to represent
+// the inventory_id value that will be passed in through the URL
+// router.get("/edit/:inv_id", utilities.checkIfAdminOrEmployee, utilities.handleErrors(invController.buildEditClassificationListItemsByIdView))
+
+// // Route for update view.
+// router.post("/update/",  
+//     regValidate.addUpdateRules(),
+//     regValidate.checkUpdateData,
+//     utilities.checkIfAdminOrEmployee, 
+//     utilities.handleErrors(invController.updateInventory))
+
+// // Route for delete action.
+// router.get("/delete/:inv_id", utilities.checkIfAdminOrEmployee, utilities.handleErrors(invController.buildDeleteConfirmationView))
+
+// // Route for processing delete option.
+// router.post("/delete/",  // No need for validation rules here, we are just retrieving and deleting data.
+//     utilities.checkIfAdminOrEmployee,
+//     utilities.handleErrors(invController.deleteItemProcessing))
+
 module.exports = router;
